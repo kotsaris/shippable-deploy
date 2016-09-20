e5f3525001be599287a4835537eae8e219c5f513
Konstantinos Konstantinidis <konstantinidis.kostas@gmail.com>
2016-09-20 23:40:13 +0100

    extracting deploy to sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+echo "Current branch is $1 and is this a pull request=?($2)"
+if [ $1 != "master" -o $2 = "true" ]
+then
+  echo "Not deploying to gh-pages"
+  exit 0
+fi
+
+echo "Deploying to gh-pages"
+git remote add deployref git@github.com:kotsaris/shippable-deploy.git
+git push deployref master:gh-pages
+exit 0
